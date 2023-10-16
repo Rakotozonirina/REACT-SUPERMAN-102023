775d02e496635aa4a669150526ab528ccaab09bd
Zo Nirina <tomzonirina@gmail.com>
2023-10-17 00:47:39 +0300

    feat: adding the footer

--- superman/src/pages/Home.jsx
+++ superman/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/homeCompenent/Header'
 import Main from '../components/homeCompenent/Main'
+import Footer from '../components/Footer'
 
 function Home() {
 return (
     <div>
         <Header/>
         <Main/>
+        <Footer/>
     </div>
     
 )
